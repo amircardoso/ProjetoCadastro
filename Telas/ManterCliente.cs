51479d69ebf347f881b1cfeea53fadca856f584b
Amir TJ Cardoso <57577175+amircardoso@users.noreply.github.com>
2022-02-28 15:53:29 -0300

    Add files via upload

---
+++ Telas/ManterCliente.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.Model;
+using Biblioteca.Arquivo;
+using _01_Projeto.Conversores;
+
+namespace _01_Projeto.Telas
+{
+    class ManterCliente
+    {
+        public void CadastrarCliente()
+        {
+            Cliente cliente = new Cliente();
+
+            Console.Clear();
+
+            Console.WriteLine("CADASTRAR CLIENTE");
+
+            Console.Write("Nome: ");
+            cliente.Nome = Console.ReadLine();
+
+            Console.Write("Data de Nascimento: ");
+            cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Telefone: ");
+            cliente.Telefone = Console.ReadLine();
+
+            Console.Write("CPF (apenas números): ");
+            cliente.CPF = int.Parse(Console.ReadLine());
+            
+            Console.Write("RG: ");
+            cliente.RG = Console.ReadLine();
+
+            Console.Write("CEP: ");
+            cliente.CEP = int.Parse(Console.ReadLine());
+
+            Console.Write("Estado: ");
+            cliente.Estado = Console.ReadLine();
+
+            Console.Write("Cidade: ");
+            cliente.Cidade = Console.ReadLine();
+
+            Console.Write("Endereco: ");
+            cliente.Endereco = Console.ReadLine();
+
+            GerenciadorArquivo.GravarArquivo("cliente", ClienteTexto.ConverterParaTexto(cliente));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+        }
+
+        public void ListarClientes ()
+        {
+            Console.Clear();
+            Console.WriteLine("LISTAGEM DE CLIENTES");
+
+            string[] linhas = GerenciadorArquivo.LerArquivo("cliente");
+
+            foreach (string linha in linhas)
+            {
+                Cliente cliente = Conversores.ClienteTexto.ConverterParaCliente(linha);
+                Console.Write("Nome: " + cliente.Nome);
+            }
+
+        }
+    }
+}
---
+++ Telas/ManterFuncionario.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.Model;
+using Biblioteca.Arquivo;
+using _01_Projeto.Conversores;
+
+namespace _01_Projeto.Telas
+{
+    class ManterFuncionario
+    {
+        public void CadastrarFuncionario ()
+        {
+           Funcionario funcionario = new Funcionario();
+
+            Console.Clear();
+
+            Console.WriteLine("CADASTRAR FUNCIONARIO");
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Data de Nascimento: ");
+            funcionario.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Telefone: ");
+            funcionario.Telefone = Console.ReadLine();
+
+            Console.Write("CPF (apenas números): ");
+            funcionario.CPF = int.Parse(Console.ReadLine());
+
+            Console.Write("RG: ");
+            funcionario.RG = Console.ReadLine();
+
+            Console.Write("CEP: ");
+            funcionario.CEP = int.Parse(Console.ReadLine());
+
+            Console.Write("Estado: ");
+            funcionario.Estado = Console.ReadLine();
+
+            Console.Write("Cidade: ");
+            funcionario.Cidade = Console.ReadLine();
+
+            Console.Write("Endereco: ");
+            funcionario.Endereco = Console.ReadLine();
+
+            Console.Write("Cargo: ");
+            funcionario.Cargo = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            funcionario.Salario = Double.Parse(Console.ReadLine());
+
+            Console.Write("Data de Contratação: ");
+            funcionario.DataContratacao = DateTime.Parse(Console.ReadLine());
+
+            GerenciadorArquivo.GravarArquivo("funcionario", FuncionarioTexto.ConverterParaTexto(funcionario));
+        }
+
+        public void ListarFuncionarios ()
+        {
+            Console.Clear();
+            Console.WriteLine("LISTAGEM DE FUNCIONARIOS");
+
+            string[] linhas = GerenciadorArquivo.LerArquivo("funcionario");
+
+            foreach (string linha in linhas)
+            {
+                Funcionario funcionario = Conversores.FuncionarioTexto.ConverterParaFuncionario(linha);
+                Console.Write("Nome: " + funcionario.Nome + funcionario.Cargo);
+            }
+
+        }
+        
+    }
+}
